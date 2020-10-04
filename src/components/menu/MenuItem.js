9ea419960735ab87c2093da70c8d0e3823539fbe
Abishek B <abishekbalaji7.151@gmail.com>
2020-10-04 10:58:07 +0530

    Setup production build and server

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -9,7 +9,8 @@ const MenuItem = (props) => (
       <div className="menu__item__text">
         <h3 className="menu__item__title">{props.name}</h3>
         <p className="menu__item__price">
-          <i className="fas fa-rupee-sign"></i><span className="price"> {props.price}</span>{" "}
+          {<i className="fas fa-rupee-sign"></i> && props.price !== "AT MRP"}
+          <span className="price"> {props.price}</span>{" "}
         </p>
       </div>
     </div>
