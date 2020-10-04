c219d6f980012b539d84f269f8e0adbb0d59933e
Abishek B <abishekbalaji7.151@gmail.com>
2020-10-04 11:07:51 +0530

    Setup production build and server

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -4,12 +4,13 @@ import "animate.css/animate.min.css";
 
 const MenuItem = (props) => (
   <ScrollAnimation animateIn="animate__fadeInUp">
+    {console.log(typeof props.price)}
     <div className="menu__item">
       <img src={props.imgSource} className="menu__item__image" />
       <div className="menu__item__text">
         <h3 className="menu__item__title">{props.name}</h3>
         <p className="menu__item__price">
-          {<i className="fas fa-rupee-sign"></i> && props.price !== "AT MRP"}
+          {typeof props.price === "number" && <i className="fas fa-rupee-sign"></i>}
           <span className="price"> {props.price}</span>{" "}
         </p>
       </div>
